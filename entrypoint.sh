a7980ef7200e8cac0a6bedfbdfc02a2ff5c8eafb
Fossware <fossware-dev@users.noreply.github.com>
2026-03-17 16:24:09 +0200

    Fix entrypoint to chown /app/secrets for key file generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-chown -R notimail:notimail /app/config /app/data /app/logs
+chown -R notimail:notimail /app/config /app/data /app/logs /app/secrets
 exec su-exec notimail python NotiMail.py -c /app/config/config.ini "$@"
